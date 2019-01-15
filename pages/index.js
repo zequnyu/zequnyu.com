5ef89209232e2c084db3f8492f8e097d27fcf6e4
zequnyu <zequnyu11@gmail.com>
2019-01-14 21:49:31 -0800

    phone responsive layout

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,36 @@
 import Layout from '../components/Layout.js'
-import styled, { keyframes } from "styled-components"
+import styled, { ThemeProvider, keyframes } from "styled-components"
 import Link from 'next/link'
 import avatarImg from '../static/avatar.png'
 import uscImg from '../static/usc.png'
+import theme from '../components/theme.js'
 
-const moveFromLeft = keyframes`
+
+const moveFromUp = keyframes`
     from {
-        margin-left: -40px;
+        margin-top: -20px;
     }
     
     to {
-        margin-left: 0;
+        margin-top: 0;
     }
 `;
 
-const moveFromRight = keyframes`
+const moveFromUpDouble = keyframes`
     from {
-        margin-right: -40px;
+        margin-top: -40px;
     }
     
     to {
-        margin-right: 0;
+        margin-top: 0;
     }
 `;
 
 const Content = styled.div`
-    max-width: 960px;
-    min-width: 700px;
+    //max-width: 960px;
+    //min-width: 700px;
     margin: 0 auto;
-    height: 600px;
+    height: 60vh;
     
     display: flex;
     flex-flow: column nowrap;
@@ -38,10 +40,19 @@ const NavBar = styled.nav`
     display: flex;
     flex-flow: row wrap;
     justify-content: space-around;
-    align-items: center;
     
+    @media (max-width: ${props => props.theme.mobile}) {
+        width: 100%;
+    }
+    @media (min-width: ${props => props.theme.mobile}) {
+        width: 60%;
+    }
+    @media (min-width: ${props => props.theme.desktop}) {
+        width: 60%;
+    }
+    
+    align-items: center;
     margin: 0 auto;
-    width: 60%;
     
     font-family: 'Open Sans', sans-serif;
     color: #666666;
@@ -59,13 +70,26 @@ const LinkText = styled.a`
         -moz-transition: all 0.3s linear;
         opacity: 0.8;
     }
+    
+    @media (max-width: ${props => props.theme.mobile}) {
+        font-size: 1.5em;
+    }
 `;
 
 const AboutDiv = styled.div`
     flex: 1;
     
     display: flex;
-    flex-flow: row wrap;
+    
+    @media (max-width: ${props => props.theme.mobile}) {
+        flex-flow: column nowrap;
+    }
+    @media (min-width: ${props => props.theme.mobile}) {
+        flex-flow: row wrap;
+    }
+    @media (min-width: ${props => props.theme.desktop}) {
+        flex-flow: row wrap;
+    }
 `;
 
 const AvatarDiv = styled.div`
@@ -74,7 +98,18 @@ const AvatarDiv = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding-bottom: 40px;
+    
+    
+    
+    @media (max-width: ${props => props.theme.mobile}) {
+        padding-top: 25px;
+    }
+    @media (min-width: ${props => props.theme.mobile}) {
+        padding-bottom: 40px;
+    }
+    @media (min-width: ${props => props.theme.desktop}) {
+        padding-bottom: 40px;
+    }
 `;
 
 const Avatar = styled.img`
@@ -84,9 +119,20 @@ const Avatar = styled.img`
     
     box-shadow: 0 0 20px rgba(172,168,90,0.2);
     
-    animation: ${moveFromLeft} .5s ease-out;
-    -webkit-animation: ${moveFromLeft} .5s ease-out;
-    -moz-animation: ${moveFromLeft} .5s ease-out;
+    @media (max-width: ${props => props.theme.mobile}) {
+        align-items: center;
+        animation: ${moveFromUp} .5s ease-out;
+    }
+    @media (min-width: ${props => props.theme.mobile}) {
+        align-items: start;
+        animation: ${moveFromUpDouble} .5s ease-out;
+    }
+    @media (min-width: ${props => props.theme.desktop}) {
+        align-items: start;
+        animation: ${moveFromUpDouble} .5s ease-out;
+        
+    }
+    animation: ${moveFromUp} .5s ease-out;
 `;
 
 const InfoDiv = styled.div`
@@ -95,18 +141,27 @@ const InfoDiv = styled.div`
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
-    align-items: start;
-    padding-bottom: 60px;
     
-    animation: ${moveFromRight} .5s ease-out;
-    -webkit-animation: ${moveFromRight} .5s ease-out;
-    -moz-animation: ${moveFromRight} .5s ease-out;
+    @media (max-width: ${props => props.theme.mobile}) {
+        align-items: center;
+        animation: ${moveFromUp} .5s ease-out;
+    }
+    @media (min-width: ${props => props.theme.mobile}) {
+        align-items: start;
+        animation: ${moveFromUpDouble} .5s ease-out;
+    }
+    @media (min-width: ${props => props.theme.desktop}) {
+        animation: ${moveFromUpDouble} .5s ease-out;
+        align-items: start;
+    }
+    
+    padding-bottom: 60px;
 `;
 
 const HiText = styled.h1`
     font-family: 'Francois One', sans-serif;
     font-size: 2.4em;
-    color: #333333;
+    color: ${props => props.theme.gray3};
     
     #darkred {
         color: #8e191c;
@@ -117,6 +172,8 @@ const StatusSpan = styled.span`
     display: flex;
     flex-flow: row nowrap;
     align-items: center;
+    
+    
 `;
 
 const StatusText = styled.h3`
@@ -150,13 +207,13 @@ const SocialLink = styled.a`
     &:hover {
         color: #999999;
         transition: all 0.3s linear;
-        -webkit-transition: all 0.3s linear;
-        -moz-transition: all 0.3s linear;
+    }
+    
+    @media (max-width: ${props => props.theme.mobile}) {
+        font-size: 48px;
     }
     
     transition: all 0.3s linear;
-    -webkit-transition: all 0.3s linear;
-    -moz-transition: all 0.3s linear;
 `;
 
 class Index extends React.Component {
@@ -165,50 +222,52 @@ class Index extends React.Component {
     }
     render() {
         return (
-            <Layout>
-                <Content>
-                    <NavBar>
-                        <Link href={`/project`}>
-                            <LinkText>PROJECT</LinkText>
-                        </Link>
-                        {/*<Link href={`/code`}>*/}
-                            {/*<LinkText>CODE</LinkText>*/}
-                        {/*</Link>*/}
-                        <Link href={`/static/cv.pdf`}>
-                            <LinkText>CV</LinkText>
-                        </Link>
-                        {/*<Link href={`/writing`}>*/}
-                            {/*<LinkText>WRITING</LinkText>*/}
-                        {/*</Link>*/}
-                    </NavBar>
-                    <AboutDiv>
-                        <AvatarDiv>
-                            <Avatar src={avatarImg} alt="Avatar" />
-                        </AvatarDiv>
-                        <InfoDiv>
-                            <HiText><span id="darkred">Hi,</span> I'm Anthony</HiText>
-                            <StatusSpan>
-                                <StatusText>Currently a MSc student at </StatusText>
-                                <USCLogo src={uscImg} alt="USCLogo" />
-                            </StatusSpan>
-                            <SocialIconDiv>
-                                <SocialLink href={`https://facebook.com/anthonyyu.11`}>
-                                    <i className="fab fa-facebook-square" />
-                                </SocialLink>
-                                <SocialLink href={`https://github.com/zequnyu`}>
-                                    <i className="fab fa-github-square" />
-                                </SocialLink>
-                                <SocialLink href={`https://www.linkedin.com/in/anthony-yu-015266145/`}>
-                                    <i className="fab fa-linkedin" />
-                                </SocialLink>
-                                <SocialLink href={`mailto:zequnyu11@gmail.com`}>
-                                    <i className="fas fa-envelope-square"/>
-                                </SocialLink>
-                            </SocialIconDiv>
-                        </InfoDiv>
-                    </AboutDiv>
-                </Content>
-            </Layout>
+            <ThemeProvider theme={theme}>
+                <Layout>
+                    <Content>
+                        <NavBar>
+                            <Link href={`/project`}>
+                                <LinkText>PROJECT</LinkText>
+                            </Link>
+                            {/*<Link href={`/code`}>*/}
+                                {/*<LinkText>CODE</LinkText>*/}
+                            {/*</Link>*/}
+                            <Link href={`/static/cv.pdf`}>
+                                <LinkText>CV</LinkText>
+                            </Link>
+                            {/*<Link href={`/writing`}>*/}
+                                {/*<LinkText>WRITING</LinkText>*/}
+                            {/*</Link>*/}
+                        </NavBar>
+                        <AboutDiv>
+                            <AvatarDiv>
+                                <Avatar src={avatarImg} alt="Avatar" />
+                            </AvatarDiv>
+                            <InfoDiv>
+                                <HiText><span id="darkred">Hi,</span> I'm Anthony</HiText>
+                                <StatusSpan>
+                                    <StatusText>Currently a MSc student at </StatusText>
+                                    <USCLogo src={uscImg} alt="USCLogo" />
+                                </StatusSpan>
+                                <SocialIconDiv>
+                                    <SocialLink href={`https://facebook.com/anthonyyu.11`}>
+                                        <i className="fab fa-facebook-square" />
+                                    </SocialLink>
+                                    <SocialLink href={`https://github.com/zequnyu`}>
+                                        <i className="fab fa-github-square" />
+                                    </SocialLink>
+                                    <SocialLink href={`https://www.linkedin.com/in/anthony-yu-015266145/`}>
+                                        <i className="fab fa-linkedin" />
+                                    </SocialLink>
+                                    <SocialLink href={`mailto:zequnyu11@gmail.com`}>
+                                        <i className="fas fa-envelope-square"/>
+                                    </SocialLink>
+                                </SocialIconDiv>
+                            </InfoDiv>
+                        </AboutDiv>
+                    </Content>
+                </Layout>
+            </ThemeProvider>
         );
     }
 }
