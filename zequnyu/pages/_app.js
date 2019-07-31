b7ec7c51e3f63f9a00954b9e1c73db8cae5bd803
zequnyu <zequnyu11@gmail.com>
2019-07-31 10:42:58 -0700

    Fix _app bug when build

--- zequnyu/pages/_app.js
+++ zequnyu/pages/_app.js
@@ -6,16 +6,6 @@ import Footer from '../components/Footer'
 
 
 class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
-
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return { pageProps };
-    }
-
     render() {
         const { Component, pageProps } = this.props;
 
